e41da1cc83714ba8662816d8e64d73fdd0404053
Wemerson Jesus <wejesuss@gmail.com>
2020-06-28 19:17:49 -0300

    Add index of the posts in the home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,34 @@
 import React from "react"
+import { graphql } from 'gatsby'
 
-export default function Home() {
-  return <div>Hello world!</div>
+export default function Home({ data: { allMarkdownRemark } }) {
+  const { nodes } = allMarkdownRemark
+
+  return (
+    <>
+      <div>Hello world!</div>
+      <div>
+        <ul>
+          {nodes.map(node => (
+            <a key={node.id} href={`http://localhost:8000${node.fields.slug}`}>
+              <li>{node.fields.slug.replace(/[/\\]/g, "")}</li>
+            </a>
+          ))}
+        </ul>
+      </div>
+    </>
+  );
 }
+
+export const query = graphql`
+  query MyQuery {
+    allMarkdownRemark {
+      nodes {
+        fields {
+          slug
+        }
+        id
+      }
+    }
+  }
+`
