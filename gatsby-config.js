47084f78e0c766962d5464247327eefe6235ec1b
Wemerson Jesus <wejesuss@gmail.com>
2020-06-28 18:22:49 -0300

    Configure base directory and transformer remark

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,14 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: "posts",
+        path: `${__dirname}/blog/`
+      }
+    },
+    `gatsby-transformer-remark`
+  ],
 }
